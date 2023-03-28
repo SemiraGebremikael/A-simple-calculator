66d7151765ae5a6dc5d87757f8e308bd6da7dcbf
semira Gebremikael <99189415+semu150@users.noreply.github.com>
2023-03-28 17:30:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+
+namespace MiniCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, result;
+            string operation;
+
+            Console.WriteLine("Enter the first number:");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Choose operation (+, -, *, /):");
+            operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    Console.WriteLine($"The result of {num1} + {num2} = {result}");
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine($"The result of {num1} - {num2} = {result}");
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    Console.WriteLine($"The result of {num1} * {num2} = {result}");
+                    break;
+                case "/":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine($"The result of {num1} / {num2} = {result}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Division by zero is not allowed.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation.");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
